74bd891757a216e3f05c6487ff10dae74344168d
Habby Olusesi <habbyolu@gmail.com>
2020-12-03 23:24:07 -0500

    fixing heroku/jaws/sql connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+      host: "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      port: 3306,
+      user: "t5aftnwd76fsajoe",
+      password: "q6pmh3mvqz7yqcu2",
+      database: "bsaveltrmd1rw2d6"
+    })
+};
 
 // Makes the connection between mysql and node
 connection.connect(function(err) {
